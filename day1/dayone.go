ce07be298fac2037384a91cf93cbb7dfe21eea84
Matthew Parsons <matthew.parsons@thinkmoney.co.uk>
2024-12-02 07:40:33 +0000

    Soltiuon part 2 - calculate the number of occurances from left list to right list

--- day1/dayone.go
+++ day1/dayone.go
@@ -15,6 +15,21 @@ func soltionPartOne(leftList []int, rightList []int) int {
 	return totalDiff
 }
 
+func soltionPartTwo(leftList []int, rightList []int) int {
+	rightListMap := make(map[int]int, len(rightList))
+	// Get the right list counts of existance
+	for _, num := range rightList {
+		rightListMap[num]++
+	}
+	answer := 0
+	for _, num := range leftList {
+		if count, ok := rightListMap[num]; ok {
+			answer += num * count
+		}
+	}
+	return answer
+}
+
 func absolute(i int) int {
 	if i < 0 {
 		return -i
--- day1/dayone_test.go
+++ day1/dayone_test.go
@@ -24,12 +24,42 @@ func TestSolutionPartOne(t *testing.T) {
 			desc:           "main question",
 			leftList:       LeftList,
 			rightList:      RightList,
-			expectedOutput: 0,
+			expectedOutput: 1889772,
 		},
 	}
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
 			output := soltionPartOne(tC.leftList, tC.rightList)
+
+			assert.Equal(t, tC.expectedOutput, output, "solution output confirmtion")
+		})
+	}
+}
+
+func TestSolutionPartTwo(t *testing.T) {
+
+	testCases := []struct {
+		desc           string
+		leftList       []int
+		rightList      []int
+		expectedOutput int
+	}{
+		{
+			desc:           "example",
+			leftList:       []int{3, 4, 2, 1, 3, 3},
+			rightList:      []int{4, 3, 5, 3, 9, 3},
+			expectedOutput: 31,
+		},
+		{
+			desc:           "main question",
+			leftList:       LeftList,
+			rightList:      RightList,
+			expectedOutput: 23228917,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			output := soltionPartTwo(tC.leftList, tC.rightList)
 
 			assert.Equal(t, tC.expectedOutput, output, "solution output confirmtion")
 		})
